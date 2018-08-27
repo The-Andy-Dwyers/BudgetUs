7cf2b9b0227ee967eff0ebeb160861aa93df5bf7
Kierra Holmes <Macce@Kierras-MacBook-Pro.local>
2018-08-27 12:23:26 -0500

    update duplicate

--- server/Ctrl/incomeCtrl.js
+++ server/Ctrl/incomeCtrl.js
@@ -11,21 +11,10 @@ const getIncome = (req, res) => {
     });
 };
 
-  const addIncome = (req, res) => {
-    const db = req.app.get('db');
-    const { amount, name, users_id } = req.body;
-    console.log(req.body)
-  
-    db.income
-      .add_income([amount, name, users_id])
-      .then(response => {
-        res.status(200).send(response);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).send(err);
-      });
-  };
+const addIncome = (req, res) => {
+  const db = req.app.get("db");
+  const { amount, name, users_id } = req.body;
+  console.log(req.body);
 
   db.income
     .add_income([amount, name, users_id])
