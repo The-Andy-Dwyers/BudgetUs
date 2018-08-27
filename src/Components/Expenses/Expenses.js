c391e3f2579403e543918b9572b7f9a8a75db56d
Tim Biles <timbilestimbiles@gmail.com>
2018-08-27 09:36:52 -0500

    change test

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,14 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 import './Expenses.css';
 
 export default class Expenses extends Component {
+  componentDidMount() {
+    this.getExpenses();
+  }
 
-    render(){
-        return (
-            <div className='expenses'>
-                <h1>Expenses</h1>
-            </div>
-        )
-    }
+  getExpenses = () => {
+    axios.get('/api/expenses').then(res => {
+      console.log(res);
+    });
+  };
+
+  render() {
+    return (
+      <div className="expenses">
+        <h1>Expenses</h1>
+      </div>
+    );
+  }
 }
---
+++ src/Components/Setup/Setup.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import './Setup.css';
+
+export default class Setup extends Component {
+
+    render(){
+        return (
+            <div className='setup'>
+                <h1>Welcome to Budget</h1>
+                <h2>Let's get started!</h2>
+            </div>
+        )
+    }
+}
