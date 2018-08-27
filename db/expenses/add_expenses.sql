fd9161abb34861ddda215bd7a2756d9cba8be4a5
joshmg88 <joshmg88@gmail.com>
2018-08-27 14:18:25 -0500

    able to add expense

--- db/expenses/add_expenses.sql
+++ db/expenses/add_expenses.sql
@@ -1,2 +1,3 @@
-insert into expenses (name, amount, type, date, company)
-values ($1,$2,$3,$4,$5)
+insert into expenses (name, amount, type, date,company,category)
+values ($1,$2,$3,$4,$5,$6)
+returning *
