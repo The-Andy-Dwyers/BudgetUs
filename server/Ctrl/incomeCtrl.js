5569d988dd96b7efcab44b30caab51eaa69ab862
Kierra Holmes <Macce@Kierras-MacBook-Pro.local>
2018-08-27 12:08:26 -0500

    updated chart

--- server/Ctrl/incomeCtrl.js
+++ server/Ctrl/incomeCtrl.js
@@ -1,33 +1,32 @@
 const getIncome = (req, res) => {
-    const db = req.app.get('db');
-  
-    db.income
-      .get_income()
-      .then(response => {
-        res.status(200).send(response);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).send(err);
-      });
-  };
+  const db = req.app.get("db");
+  db.income
+    .get_income([req.user.id])
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(err);
+    });
+};
 
-  const addIncome = (req, res) => {
-    const db = req.app.get('db');
-    const { amount, name, users_id } = req.body;
-  
-    db.income
-      .add_income([amount, name, users_id])
-      .then(response => {
-        res.status(200).send(response);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).send(err);
-      });
-  };
+const addIncome = (req, res) => {
+  const db = req.app.get("db");
+  const { amount, name, users_id } = req.body;
 
-  module.exports = {
-      getIncome,
-      addIncome
-  }
+  db.income
+    .add_income([amount, name, users_id])
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(err);
+    });
+};
+
+module.exports = {
+  getIncome,
+  addIncome
+};
