8e4d3cc2f4f7152a1bacc5603d83af6a5fe72cbb
Tim Biles <timbilestimbiles@gmail.com>
2018-08-27 21:21:04 -0500

    merging

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
 import { Doughnut } from "react-chartjs-2";
 import { connect } from "react-redux";
-import { getExpensesByCategory } from "../../ducks/reducers/expensesReducer";
 
 class Chart extends Component {
-  componentDidMount() {
-    this.props.getExpensesByCategory();
-  }
   render() {
-    console.log(this.props);
+    console.log(this.props.income);
     const incomedata = {
       datasets: [
         {
@@ -22,47 +18,28 @@ class Chart extends Component {
     };
     const spenddata = {
       datasets: [
-        {
-          data: this.props.expenses.map(e => e.amount),
-          backgroundColor: ["blue", "green", "purple", "red"]
-        }
+        { data: [5, 250], backgroundColor: ["blue", "green", "purple"] }
       ],
-      labels: this.props.expenses.map(e => e.category)
+      labels: ["label one", "ltwo", "third"]
     };
     const options = {
-      tooltips: {
-        callbacks: {
-          label: function(tooltipItem, data) {
-            console.log("~~~*** tooltip", tooltipItem);
-            console.log("~~~~***data", data);
-            return "$" + data.datasets[0]["data"][tooltipItem.index];
-          }
-        }
-      },
       legend: { display: true, labels: { fontColor: "black" } }
     };
     return (
       <div>
         <h2>Income Data</h2>
-        {this.props.income.length !== 0 &&
-        <Doughnut data={incomedata} />
-        }
+        <Doughnut data={incomedata} options={options} />
         <h2>Expenses Data</h2>
-        {this.props.expenses.length !== 0 && (
-          <Doughnut data={spenddata} options={options} />
-        )}
+        <Doughnut data={spenddata} options={options} />
       </div>
     );
   }
 }
 function mapStateToProps(state) {
   return {
-    income: state.incomeReducer.income,
-    expenses: state.expensesReducer.expensesbycat
+    income: state.incomeReducer.income
   };
 }
 
-export default connect(
-  mapStateToProps,
-  { getExpensesByCategory, }
-)(Chart);
+// export default Charts;
+export default connect(mapStateToProps)(Chart);
