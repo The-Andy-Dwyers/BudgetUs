623ab7aec1fc194ccb6c939d860538eafc4353ae
joshmg88 <joshmg88@gmail.com>
2018-08-28 16:58:58 -0500

    stylin

--- src/Components/Expenses/ExpensesInfo.js
+++ src/Components/Expenses/ExpensesInfo.js
@@ -10,9 +10,23 @@ import { getUsers } from "../../ducks/reducers/userReducer";
 import DatePicker from "react-custom-date-picker";
 import moment from "moment";
 import axios from "axios";
+import Modal from "react-modal";
 // import Chart from "../Chart/Chart";
 
 import "./Expenses.css";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)"
+  }
+};
+
+Modal.setAppElement(document.getElementById("root"));
 
 class Expenses extends Component {
   state = {
@@ -21,7 +35,17 @@ class Expenses extends Component {
     type: "",
     company: "",
     category: "",
-    date: new Date().toISOString()
+    date: new Date().toISOString(),
+    modalIsOpen: false
+  };
+
+  openModal = () => {
+    this.setState({ modalIsOpen: true });
+    console.log("open");
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
   };
 
   componentDidMount() {
@@ -61,91 +85,106 @@ class Expenses extends Component {
       this.props.expensesReducer.expense.map(e => {
         return (
           <div className="Expenses_expenselist" key={e.id}>
-            <p>{e.name}</p>
-            <p>{e.amount}</p>
-            <p>{moment.utc(date).format("ddd, MMM D")}</p>
-            <p>{e.type}</p>
-            <p>{e.company}</p>
-            <p>{e.category}</p>
+            <p className="Expenses_list_content">{e.name}</p>
+            <p className="Expenses_list_content">{e.amount}</p>
+            <p className="Expenses_list_content">
+              {moment.utc(date).format("ddd, MMM D")}
+            </p>
+            <p className="Expenses_list_content">{e.type}</p>
+            <p className="Expenses_list_content">{e.company}</p>
+            <p className="Expenses_list_content">{e.category}</p>
             <button onClick={id => this.handleDelete(e.id)}>Delete</button>
           </div>
         );
       });
     return (
-      <div className="expenses">
-        <h1>Expenses</h1>
-        <input
-          className="Expenses_input"
-          placeholder="expense name"
-          onChange={e => this.handleInputs(e.target.value, "expenseName")}
-        />
-        <input
-          className="Expenses_input"
-          placeholder="amount"
-          onChange={e => this.handleInputs(e.target.value, "amount")}
-        />
-        <DatePicker
-          date={this.state.date}
-          handleDateChange={this.handleDateChange}
-        />
-        <input
-          className="Expenses_input"
-          placeholder="Company"
-          onChange={e => this.handleInputs(e.target.value, "company")}
-        />
+      <div className="expenses_container">
+        <div className="expenses">
+          <h1 className="expense_input_btn btn" onClick={this.openModal}>
+            Add ExpenseExpense
+          </h1>
+          {map}
+        </div>
 
-        <form>
-          <input
-            name="type"
-            type="radio"
-            value="recurring"
-            onClick={() => this.handleType("Recurring")}
-          />{" "}
-          Recurring
-          <input
-            name="type"
-            type="radio"
-            value="nonrecurring"
-            onClick={() => this.handleType("Non-Recurring")}
-          />{" "}
-          Non-Recurring
-        </form>
-
-        <select
-          required
-          onChange={e => this.handleInputs(e.target.value, "category")}
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
         >
-          <option>Select Category:</option>
-          <option value="Rent">Rent</option>
-          <option value="Bills">Bills</option>
-          <option value="Food">Food</option>
-          <option value="Gas">Gas</option>
-          <option value="Entertainment">Entertainment</option>
-          <option value="Other">other</option>
-        </select>
+          <h1>Expenses</h1>
+          <input
+            className="Expenses_input"
+            placeholder="expense name"
+            onChange={e => this.handleInputs(e.target.value, "expenseName")}
+          />
+          <input
+            className="Expenses_input"
+            placeholder="amount"
+            onChange={e => this.handleInputs(e.target.value, "amount")}
+          />
+          <DatePicker
+            date={this.state.date}
+            handleDateChange={this.handleDateChange}
+          />
+          <input
+            className="Expenses_input"
+            placeholder="Company"
+            onChange={e => this.handleInputs(e.target.value, "company")}
+          />
 
-        <button
-          onClick={() =>
-            this.props
-              .addExpenses({
-                expenseName,
-                amount,
-                type,
-                date,
-                company,
-                category,
-                id
-              })
-              .then(() => {
-                this.props.getExpensesByCategory();
-                this.props.getExpenses();
-              })
-          }
-        >
-          Submit
-        </button>
+          <form>
+            <input
+              name="type"
+              type="radio"
+              value="recurring"
+              onClick={() => this.handleType("Recurring")}
+            />{" "}
+            Recurring
+            <input
+              name="type"
+              type="radio"
+              value="nonrecurring"
+              onClick={() => this.handleType("Non-Recurring")}
+            />{" "}
+            Non-Recurring
+          </form>
 
-        {map}
+          <select
+            required
+            onChange={e => this.handleInputs(e.target.value, "category")}
+          >
+            <option>Select Category:</option>
+            <option value="Rent">Rent</option>
+            <option value="Bills">Bills</option>
+            <option value="Food">Food</option>
+            <option value="Gas">Gas</option>
+            <option value="Entertainment">Entertainment</option>
+            <option value="Other">other</option>
+          </select>
+
+          <button
+            onClick={() =>
+              this.props
+                .addExpenses({
+                  expenseName,
+                  amount,
+                  type,
+                  date,
+                  company,
+                  category,
+                  id
+                })
+                .then(() => {
+                  this.closeModal();
+                  this.props.getExpensesByCategory();
+                  this.props.getExpenses();
+                })
+            }
+          >
+            Submit
+          </button>
+        </Modal>
       </div>
     );
   }
