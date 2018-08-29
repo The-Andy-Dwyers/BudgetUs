1d245418e88e9e291fc5b30ffcf690abdc820926
Tim Biles <timbilestimbiles@gmail.com>
2018-08-29 15:18:12 -0500

    bringing in changes

---
+++ src/Components/Goals/Goals.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Goals extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Set your goals for this month!</h1>
+                <h3>Monthly savings</h3>
+            </div>
+        );
+    }
+}
+
+export default Goals;
