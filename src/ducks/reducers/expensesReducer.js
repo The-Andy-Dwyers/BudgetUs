bff2684a69be95e1670e3d6e5a684f709fa6ed9e
Tim Biles <timbilestimbiles@gmail.com>
2018-08-29 16:57:47 -0500

    end of day merge

--- src/ducks/reducers/expensesReducer.js
+++ src/ducks/reducers/expensesReducer.js
@@ -1,34 +1,41 @@
-import axios from "axios";
+import axios from 'axios';
 
-const GET_EXPENSES = "GET_EXPENSES";
-const GET_EXPENSES_BY_CAT = "GET_EXPENSES_BY_CAT";
-const ADD_EXPENSE = "ADD_EXPENSE";
-const DELETE_EXPENSE = "DELETE_EXPENSE";
+const GET_EXPENSES = 'GET_EXPENSES';
+const GET_EXPENSES_BY_CAT = 'GET_EXPENSES_BY_CAT';
+const ADD_EXPENSE = 'ADD_EXPENSE';
+const DELETE_EXPENSE = 'DELETE_EXPENSE';
 
 export function getExpenses() {
   return {
-    type: "GET_EXPENSES",
+    type: 'GET_EXPENSES',
     payload: axios.get(`/api/expenses`)
-
   };
 }
+
 export function getExpensesByCategory() {
   return {
-    type: "GET_EXPENSES_BY_CAT",
+    type: 'GET_EXPENSES_BY_CAT',
     payload: axios.get(`/api/expenses_by_cat`)
+  };
+}
+
+export function getYearlyExpensesByCategory() {
+  return {
+    type: 'GET_EXPENSES_BY_CAT',
+    payload: axios.get(`/api/yearly-expenses_by_cat`)
   };
 }
 
 export function addExpenses(newExpense) {
   return {
-    type: "ADD_EXPENSE",
-    payload: axios.post("/api/add-expenses", newExpense)
+    type: 'ADD_EXPENSE',
+    payload: axios.post('/api/add-expenses', newExpense)
   };
 }
 
 export function deleteExpense(expense) {
   return {
-    type: "DELETE_EXPENSE",
+    type: 'DELETE_EXPENSE',
     payload: axios.delete(`/api/delete-expense/${expense.id}`)
   };
 }
