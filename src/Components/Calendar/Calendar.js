8715fb2bdc3a4c1233997efba7962f341e50cecd
Kierra Holmes <Macce@Kierras-MacBook-Pro.local>
2018-08-31 13:35:40 -0500

    finished line chart

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -1,36 +1,39 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import 'fullcalendar';
-import $ from 'jquery';
-import moment from 'moment';
-import './Calendar.css';
-import './fullCalendar.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "fullcalendar";
+import $ from "jquery";
+import moment from "moment";
+import "./Calendar.css";
+import "./fullCalendar.css";
 
-import { getUsers } from '../../ducks/reducers/userReducer';
+import { getUsers } from "../../ducks/reducers/userReducer";
 
 class Calendar extends Component {
-
   state = {
-    date: 'Hi there'
-  }
-
-
+    date: "Hi there"
+  };
 
   componentDidMount() {
     this.props.getUsers();
 
-    $('#calendar').fullCalendar({
+    $("#calendar").fullCalendar({
       header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay'
+        left: "prev,next today",
+        center: "title",
+        right: "month,agendaWeek,agendaDay"
       },
       editable: true,
       droppable: true, // this allows things to be dropped onto the calendar
       eventDrop: function(date, dayDelta, minuteDelta, allDay) {
-        const dateDrop = moment(date.start._d).add(1, 'day').format('YYYY-MM-DD')
-        console.log(moment(date.start._d).add(1, 'day').format('YYYY-MM-DD'));
-        console.log(allDay)
+        const dateDrop = moment(date.start._d)
+          .add(1, "day")
+          .format("YYYY-MM-DD");
+        console.log(
+          moment(date.start._d)
+            .add(1, "day")
+            .format("YYYY-MM-DD")
+        );
+        console.log(allDay);
         // const chageEvent = () => {
 
         //   this.setState({date: dateDrop})
@@ -40,38 +43,40 @@ class Calendar extends Component {
 
       // events: `/api/income/${this.props.userReducer.id}`,
       events: `/api/income/6`,
-      
+
       eventMouseover: function(e, jsEvent) {
+        console.log(e);
         const tooltip =
           '<div class="tooltipevent" style="padding:1% 1.5%;opacity: 0.8;background:rgb(241, 241, 241);position:absolute;z-index:10001;">' +
           e.title +
-          '<br />' +
-          '$' + e.amount +
-          '</div>';
-        $('body').append(tooltip);
+          "<br />" +
+          "$" +
+          e.amount +
+          "</div>";
+        $("body").append(tooltip);
         $(this)
           .mouseover(function(e) {
-            $(this).css('z-index', 10000);
-            $('.tooltipevent').fadeIn('500');
-            $('.tooltipevent').fadeTo('10', 1.9);
+            $(this).css("z-index", 10000);
+            $(".tooltipevent").fadeIn("500");
+            $(".tooltipevent").fadeTo("10", 1.9);
           })
           .mousemove(function(e) {
-            $('.tooltipevent').css('top', e.pageY + 10);
-            $('.tooltipevent').css('left', e.pageX + 20);
+            $(".tooltipevent").css("top", e.pageY + 10);
+            $(".tooltipevent").css("left", e.pageX + 20);
           });
       },
       eventMouseout: function(calEvent, jsEvent) {
-        $(this).css('z-index', 8);
-        $('.tooltipevent').remove();
+        $(this).css("z-index", 8);
+        $(".tooltipevent").remove();
       }
     });
   }
 
   render() {
-    console.log(this.state)
+    console.log(this.state);
     return (
       <div className="calendar">
-      <h1>Calendar</h1>
+        <h1>Calendar</h1>
         <div id="calendar" />
       </div>
     );
--- src/ducks/reducers/expensesReducer.js
+++ src/ducks/reducers/expensesReducer.js
@@ -1,49 +1,57 @@
-import axios from 'axios';
+import axios from "axios";
 
-const GET_EXPENSES = 'GET_EXPENSES';
-const GET_EXPENSES_BY_CAT = 'GET_EXPENSES_BY_CAT';
-const ADD_EXPENSE = 'ADD_EXPENSE';
-const DELETE_EXPENSE = 'DELETE_EXPENSE';
-const GET_GOAL = 'GET_GOAL';
+const GET_EXPENSES = "GET_EXPENSES";
+const GET_EXPENSES_BY_CAT = "GET_EXPENSES_BY_CAT";
+const GET_EXPENSES_BY_MONTH = "GET_EXPENSES_BY_MONTH";
+const ADD_EXPENSE = "ADD_EXPENSE";
+const DELETE_EXPENSE = "DELETE_EXPENSE";
+const GET_GOAL = "GET_GOAL";
+
+export function getExpenseTotalsByMonth() {
+  return {
+    type: "GET_EXPENSES_BY_MONTH",
+    payload: axios.get(`/api/linechart`)
+  };
+}
 
 export function getExpenses() {
   return {
-    type: 'GET_EXPENSES',
+    type: "GET_EXPENSES",
     payload: axios.get(`/api/expenses`)
   };
 }
 
 export function getExpensesByCategory() {
   return {
-    type: 'GET_EXPENSES_BY_CAT',
+    type: "GET_EXPENSES_BY_CAT",
     payload: axios.get(`/api/expenses_by_cat`)
   };
 }
 
 export function getYearlyExpensesByCategory() {
   return {
-    type: 'GET_EXPENSES_BY_CAT',
+    type: "GET_EXPENSES_BY_CAT",
     payload: axios.get(`/api/yearly-expenses_by_cat`)
   };
 }
 
 export function addExpenses(newExpense) {
   return {
-    type: 'ADD_EXPENSE',
-    payload: axios.post('/api/add-expenses', newExpense)
+    type: "ADD_EXPENSE",
+    payload: axios.post("/api/add-expenses", newExpense)
   };
 }
 
 export function deleteExpense(expense) {
   return {
-    type: 'DELETE_EXPENSE',
+    type: "DELETE_EXPENSE",
     payload: axios.delete(`/api/delete-expense/${expense.id}`)
   };
 }
 
 export function getGoals() {
   return {
-    type: 'GET_GOAL',
+    type: "GET_GOAL",
     payload: axios.get(`/api/goal`)
   };
 }
@@ -52,6 +60,7 @@ const initialState = {
   expense: [],
   goals: [],
   expensesbycat: [],
+  expensesbymonth: [],
   didErr: false
 };
 
@@ -68,6 +77,8 @@ export default function expensesReducer(state = initialState, action) {
       };
     case `${GET_EXPENSES_BY_CAT}_FULFILLED`:
       return { ...state, expensesbycat: action.payload.data };
+    case `${GET_EXPENSES_BY_MONTH}_FULFILLED`:
+      return { ...state, expensesbymonth: action.payload.data };
     case `${GET_EXPENSES}_REJECTED`:
     case `${ADD_EXPENSE}_REJECTED`:
     case `${GET_EXPENSES_BY_CAT}_REJECTED`:
