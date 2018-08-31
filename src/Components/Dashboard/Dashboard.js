e3b15b4cb8a406c17b9afa893f4ecd102d36b090
Kierra Holmes <Macce@Kierras-MacBook-Pro.local>
2018-08-31 14:55:16 -0500

    line chart

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import axios from 'axios';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import axios from "axios";
 // import moment from 'moment';
-import Switch from 'react-switch';
-import { Link } from 'react-router-dom';
+import Switch from "react-switch";
+import { Link } from "react-router-dom";
 
-import Income from '../Income/Income';
-import Chart from '../Chart/Chart';
-import Goals from '../Goals/Goals';
+import Income from "../Income/Income";
+import Chart from "../Chart/Chart";
+import Goals from "../Goals/Goals";
+import LineChart from "../Chart/LineChart";
 
-import './Dashboard.css';
-import { getUsers } from '../../ducks/reducers/userReducer';
-import { getDashboard } from '../../ducks/reducers/incomeReducer';
+import "./Dashboard.css";
+import { getUsers } from "../../ducks/reducers/userReducer";
+import { getDashboard } from "../../ducks/reducers/incomeReducer";
 
 class Dashboard extends Component {
   constructor() {
@@ -22,19 +23,19 @@ class Dashboard extends Component {
     };
   }
   componentDidMount() {
-    this.props.getDashboard('month');
+    this.props.getDashboard("month");
     this.getTopExpenses();
   }
 
   getTopExpenses = () => {
-    axios.get('/api/top-expenses').then(res => {
+    axios.get("/api/top-expenses").then(res => {
       this.setState({ expenses: res.data });
     });
   };
 
   handleChange = month =>
     this.setState({ month }, () =>
-      this.props.getDashboard(this.state.month ? 'month' : 'year')
+      this.props.getDashboard(this.state.month ? "month" : "year")
     );
   render() {
     const { expenses } = this.state;
@@ -56,14 +57,14 @@ class Dashboard extends Component {
           uncheckedIcon={
             <div
               style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100%',
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
                 fontSize: 15,
-                color: 'white',
+                color: "white",
                 paddingRight: 2,
-                background: '#d12012',
+                background: "#d12012",
                 borderRadius: 50
               }}
             >
@@ -73,14 +74,14 @@ class Dashboard extends Component {
           checkedIcon={
             <div
               style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100%',
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
                 fontSize: 15,
-                color: 'white',
+                color: "white",
                 paddingRight: 2,
-                background: '#d12012',
+                background: "#d12012",
                 borderRadius: 50
               }}
             >
@@ -96,10 +97,10 @@ class Dashboard extends Component {
             <Income month={this.state.month} />
           )}
           <div className="dashboard_expense">
-          <h2>Top Expenses</h2>
+            <h2>Top Expenses</h2>
             <div>{map}</div>
             <Link className="link2" to="/expenses">
-              <h2 className='expenses_link btn' >More info</h2>
+              <h2 className="expenses_link btn">More info</h2>
             </Link>
           </div>
         </div>
@@ -107,6 +108,7 @@ class Dashboard extends Component {
         {this.props.incomeReducer.dashboard.sources && (
           <Chart type="remaining" />
         )}
+        {this.props.incomeReducer.dashboard.sources && <LineChart />}
       </div>
     );
   }
