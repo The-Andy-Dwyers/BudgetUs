98814370652d88ed401f4e01a4b91f75d281c466
Tim Biles <timbilestimbiles@gmail.com>
2018-09-04 15:32:54 -0500

    many styling changes

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
-import './Settings.css'
+import './Settings.css';
 import Setup from '../Setup/Setup';
 
 export default class Settings extends Component {
-
-    render() {
-        return (
-            <div className='settings_main'>
-                <Setup />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="settings_main">
+        <div className="settings_header" />
+        <Setup />
+      </div>
+    );
+  }
 }
